9016ec8b652017b8e828bfbaf6bfafd77aa1e778
Matteo Ciapparelli <matteo.ciapparelli@reti.it>
2023-03-02 22:07:14 +0100

    Fix 'Main'

--- Lezione01/EFBasics/EFCoreDbFirst/Program.cs
+++ Lezione01/EFBasics/EFCoreDbFirst/Program.cs
@@ -1,2 +1,4 @@
-﻿// Launch this command to generate classes from database:
+﻿Console.WriteLine("Hello World!");
+
+// Launch this command to generate classes from database:
 // Scaffold-DbContext "Data Source=.;Initial Catalog=BlogDB;Integrated Security=SSPI;MultipleActiveResultSets=True" Microsoft.EntityFrameworkCore.SqlServer
